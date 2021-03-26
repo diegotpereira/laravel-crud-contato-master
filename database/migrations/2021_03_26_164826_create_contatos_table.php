a6a0820e4f171a91dd4c76f084817c1ab607ef6b
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-26 13:54:33 -0300

    criação do arquivo tabela contatos.

---
+++ database/migrations/2021_03_26_164826_create_contatos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contatos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome');
+            $table->string('sobrenome');
+            $table->string('email');
+            $table->string('funcao');
+            $table->string('cidade');
+            $table->string('pais');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contatos');
+    }
+}
