b0b4c4ab002b8f51f2a88884c50c70317c15a3d5
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-26 14:26:37 -0300

    criação do layout base para os templates.

---
+++ resources/views/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+   <head>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>App Crud Contatos Laravel</title>
+     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+   </head>
+<body>
+    <div class="container">
+      @yield('main')
+    </div>
+    <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</body>
+</html>
